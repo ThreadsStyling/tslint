565f5854293a1bf985ceeb99f6c451d2a09ec6ef
Diogo Cunha <diogofncunha@gmail.com>
2018-08-02 10:38:32 +0100

    feat: 🎸 override no-unused-expression

--- index.js
+++ index.js
@@ -1,88 +1,77 @@
 /* tslint:disable:prefer-object-spread */
 const commonRules = {
-  'ban': [
-    true,
-    'eval',
-    '$',
-    ['describe', 'only'],
-    ['it', 'only']
-  ],
-  'ban-comma-operator': true,
-  'encoding': true,
-  'newline-before-return': true,
-  'no-duplicate-imports': true,
-  'no-duplicate-switch-case': true,
-  'no-duplicate-variable': true,
-  'no-dynamic-delete': true,
-  'no-empty': false,
-  'no-implicit-dependencies': [true, 'dev'],
-  'no-import-side-effect': [true, {'ignore-module': '\\.css$'}],
-  'no-invalid-template-strings': true,
-  'no-invalid-this': true,
-  'no-magic-numbers': false,
-  'no-parameter-reassignment': true,
-  'no-require-imports': false,
-  'no-return-await': true,
-  'no-sparse-arrays': true,
-  'no-switch-case-fall-through': true,
-  'no-this-assignment': [true, {'allow-destructuring': true}],
-  'no-unnecessary-callback-wrapper': true,
-  'no-unnecessary-class': true,
-  'object-literal-sort-keys': false,
-  'ordered-imports': [
+  ban: [true, "eval", "$", ["describe", "only"], ["it", "only"]],
+  "ban-comma-operator": true,
+  encoding: true,
+  "newline-before-return": true,
+  "no-duplicate-imports": true,
+  "no-duplicate-switch-case": true,
+  "no-duplicate-variable": true,
+  "no-dynamic-delete": true,
+  "no-empty": false,
+  "no-implicit-dependencies": [true, "dev"],
+  "no-import-side-effect": [true, { "ignore-module": "\\.css$" }],
+  "no-invalid-template-strings": true,
+  "no-invalid-this": true,
+  "no-magic-numbers": false,
+  "no-parameter-reassignment": true,
+  "no-require-imports": false,
+  "no-return-await": true,
+  "no-sparse-arrays": true,
+  "no-switch-case-fall-through": true,
+  "no-this-assignment": [true, { "allow-destructuring": true }],
+  "no-unnecessary-callback-wrapper": true,
+  "no-unnecessary-class": true,
+  "object-literal-sort-keys": false,
+  "ordered-imports": [
     true,
     {
-      'grouped-imports': true,
-      'import-sources-order': 'any',
-      'named-imports-order': 'any'
+      "grouped-imports": true,
+      "import-sources-order": "any",
+      "named-imports-order": "any"
     }
   ],
 
   // This rule does not make any sense.
-  'prefer-conditional-expression': false,
+  "prefer-conditional-expression": false,
 
-  'prefer-function-over-method': true,
+  "prefer-function-over-method": true,
 
   // Sometimes you just want to do Object.assign
-  'prefer-object-spread': false,
+  "prefer-object-spread": false,
 
-  'prefer-switch': true,
+  "prefer-switch": true,
 
   // I think we can decide for ourselves which string syntax to use.
-  'prefer-template': false,
+  "prefer-template": false,
 
-  'triple-equals': true
+  "triple-equals": true
 };
 
 module.exports = {
-  extends: ['tslint:recommended', 'tslint-config-prettier'],
+  extends: ["tslint:recommended", "tslint-config-prettier"],
   jsRules: commonRules,
   linterOptions: {
-    exclude: [
-      'node_modules',
-      'dist',
-      'coverage',
-      'dev'
-    ]
+    exclude: ["node_modules", "dist", "coverage", "dev"]
   },
   rules: Object.assign(
     {
-      'no-var-requires': false,
-      'array-type': false,
-      'member-access': false
+      "no-var-requires": false,
+      "array-type": false,
+      "member-access": false
     },
     commonRules,
     {
       // Some team members believe interface prefixing is remnants of C#.
-      'interface-name': false,
+      "interface-name": false,
 
-      'no-any': false,
-      'no-inferrable-types': true,
-      'no-non-null-assertion': true,
+      "no-any": false,
+      "no-inferrable-types": true,
+      "no-non-null-assertion": true,
 
       // Not sure if we should disable this one.
       // But I sometimes shadow variables and Forbes said he does, too.
-      'no-shadowed-variable': false,
+      "no-shadowed-variable": false,
 
       // Sometimes you need to do bitwise operations.
       "no-bitwise": false,
@@ -114,7 +103,8 @@ module.exports = {
       "member-ordering": false,
 
       // It should be fine to have more than one class per file.
-      "max-classes-per-file": false
+      "max-classes-per-file": false,
+      "no-unused-expression": false
     }
   )
 };
