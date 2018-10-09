24fa5195bf2c25cd664479a6b76e5ca7902c1df8
Forbes Lindesay <forbes@lindesay.co.uk>
2018-10-09 11:55:04 +0100

    feat: update to agreed list of rules (#9)
    BREAKING CHANGE: the rules have changed substantially, so some things  may need changing to match the new lint rules
    
    BREAKING CHANGE: there are no rules for JavaScript since we always write TypeScript

--- react.js
+++ react.js
@@ -1,18 +1,14 @@
-/* tslint:disable:prefer-object-spread */
-const commonRules = {
-  'jsx-alignment': false,
-  'jsx-equals-spacing': false,
-  'jsx-key': true,
-  'jsx-no-multiline-js': false,
-  'jsx-no-string-ref': true,
-  'jsx-self-close': true,
-  'jsx-wrap-multiline': false,
-};
-
 module.exports = {
   extends: ['tslint-react'],
-  jsRules: commonRules,
-  rules: Object.assign({}, commonRules, {
+  rules: {
+    'jsx-alignment': false,
+    'jsx-boolean-value': true,
     'jsx-curly-spacing': false,
-  }),
+    'jsx-equals-spacing': false,
+    'jsx-key': true,
+    'jsx-no-multiline-js': false,
+    'jsx-no-string-ref': true,
+    'jsx-self-close': false,
+    'jsx-wrap-multiline': false,
+  },
 };
